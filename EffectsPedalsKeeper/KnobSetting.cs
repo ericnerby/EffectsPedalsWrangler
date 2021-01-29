9cdde292986c0e00a7c74a8aaccd9a930352159a
Eric Nerby <ericnerby@msn.com>
2021-01-29 09:51:46 -0500

    created KnobSetting and KnobSettingTests classes

---
+++ EffectsPedalsKeeper/KnobSetting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper
+{
+    public class KnobSetting : ISetting
+    {
+        public string Label { get; }
+
+        public KnobSetting(string label, string minClockPosition, string maxClockPosition)
+        {
+        }
+
+        public KnobSetting(string label, int minKnobValue, int maxKnobValue)
+        {
+        }
+
+        public int CurrentValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int MinValue => throw new NotImplementedException();
+
+        public int MaxValue => throw new NotImplementedException();
+
+        public string[] Display()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int StepDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int StepUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EffectsPedalsKeeperTests/KnobSettingTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using EffectsPedalsKeeper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper.Tests
+{
+    public class KnobSettingTests
+    {
+        private KnobSetting _knobSetting;
+
+        [Fact()]
+        public void DisplayTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void StepDownTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void StepUpTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
