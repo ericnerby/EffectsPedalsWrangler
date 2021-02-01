ced3154f4494b8637f0fc29674825613b8cd758f
Eric Nerby <ericnerby@msn.com>
2021-02-01 17:04:01 -0500

    fixed SwitchSetting constructor by changing option order

--- EffectsPedalsKeeper/SwitchSetting.cs
+++ EffectsPedalsKeeper/SwitchSetting.cs
@@ -6,7 +6,8 @@ namespace EffectsPedalsKeeper
 {
     public class SwitchSetting : RotarySetting
     {
-        public SwitchSetting(string label) : base(label, new string[] { "On", "Off" })
+        public SwitchSetting(string label)
+            : base(label, new string[] { "Off", "On" })
         {}
     }
 }
