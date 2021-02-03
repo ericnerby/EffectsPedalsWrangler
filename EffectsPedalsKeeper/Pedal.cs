357bccbf2528590daa570528b8e6c6194d28a296
Eric Nerby <ericnerby@msn.com>
2021-02-03 11:13:32 -0500

    stubbed Pedal and PedalTests

---
+++ EffectsPedalsKeeper/Pedal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper
+{
+    public class Pedal
+    {
+        public bool Engaged { get; set; }
+
+        public string Maker { get; }
+        public string Name { get; }
+
+        public List<Setting> Settings { get; private set; }
+
+        public Pedal(string maker, string name)
+        { }
+
+        public bool AddSettings(IList<Setting> settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] PrintSettingDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] DisplayPedal()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ EffectsPedalsKeeperTests/PedalTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using EffectsPedalsKeeper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper.Tests
+{
+    public class PedalTests
+    {
+        [Fact()]
+        public void PedalTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void PrintSettingDetailsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DisplayPedalTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddSettingsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
