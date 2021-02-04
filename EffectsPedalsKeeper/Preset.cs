a9cd802c3045f9a9d6a25610e080c74535561948
Eric Nerby <ericnerby@msn.com>
2021-02-04 09:09:12 -0500

    create Preset and PresetTests

---
+++ EffectsPedalsKeeper/Preset.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper
+{
+    public class Preset : IList<Pedal>
+    {
+        public Pedal this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(Pedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Pedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Pedal[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Pedal> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(Pedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, Pedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Pedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EffectsPedalsKeeperTests/PresetTests.cs
@@ -0,0 +1,65 @@
+﻿using Xunit;
+using EffectsPedalsKeeper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper.Tests
+{
+    public class PresetTests
+    {
+        [Fact()]
+        public void AddTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ClearTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ContainsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CopyToTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetEnumeratorTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void IndexOfTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InsertTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveAtTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
