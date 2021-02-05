bef3fa4f62d20a78994eb2079d782823eccdb180
Eric Nerby <ericnerby@msn.com>
2021-02-05 09:12:50 -0500

    add MockPedal class (incomplete) to facilitate Preset Unit Testing

---
+++ EffectsPedalsKeeperTests/Mocks/MockPedal.cs
@@ -0,0 +1,33 @@
+﻿using EffectsPedalsKeeper;
+using EffectsPedalsKeeper.Settings;
+using System;
+using System.Collections.Generic;
+
+namespace EffectsPedalsKeeper.Tests.Mocks
+{
+    class MockPedal : IPedal
+    {
+        public bool Engaged { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Maker => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public List<ISetting> Settings => throw new NotImplementedException();
+
+        public bool AddSettings(IList<ISetting> settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddSettings(params ISetting[] settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] PrintSettingDetails()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
