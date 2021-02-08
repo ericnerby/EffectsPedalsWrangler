ee72192e80bff4f431e4039176d1671c4a08ac15
Eric Nerby <ericnerby@msn.com>
2021-02-08 12:25:14 -0500

    add Mocks.PresetMock class for PedalBoardTests

---
+++ EffectsPedalsKeeperTests/Mocks/PresetMock.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper.Tests.Mocks
+{
+    class PresetMock : IPreset
+    {
+        public IPedal this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Name => throw new NotImplementedException();
+
+        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(IPedal[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<IPedal> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
