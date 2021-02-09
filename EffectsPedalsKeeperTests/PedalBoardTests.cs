b2f56c36a558104488d3a5ce4956925dc96c87ea
Eric Nerby <ericnerby@msn.com>
2021-02-09 09:13:55 -0500

    add IPreset interface to PedalBoard and stub unit tests

--- EffectsPedalsKeeperTests/PedalBoardTests.cs
+++ EffectsPedalsKeeperTests/PedalBoardTests.cs
@@ -53,6 +53,12 @@ namespace EffectsPedalsKeeper.Tests
         }
 
         [Fact()]
+        public void SaveAsPresetTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
         public void NewPresetTest()
         {
             Assert.True(false, "This test needs an implementation");
@@ -60,6 +66,60 @@ namespace EffectsPedalsKeeper.Tests
 
         [Fact()]
         public void AddPedalsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void IndexOfTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InsertTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveAtTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ClearTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ContainsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CopyToTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetEnumeratorTest()
         {
             Assert.True(false, "This test needs an implementation");
         }
