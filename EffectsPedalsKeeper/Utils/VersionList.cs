70a44c42650f5c94721845c64a25dee05f38199c
Eric Nerby <ericnerby@msn.com>
2021-02-09 10:25:50 -0500

    stub VersionList and VersionListTests

---
+++ EffectsPedalsKeeper/Utils/VersionList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace EffectsPedalsKeeper.Utils
+{
+    /// <summary>
+    /// A List of reference type objects with version control.
+    /// The same set of objects are saved across all versions,
+    /// but their properties can be set differently.
+    /// </summary>
+    /// <typeparam name="T">Type T must be a reference type</typeparam>
+    public class VersionList<T> : IList<T> where T : class
+    {
+        public void CheckOutVersion(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveVersion()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveAsVersion(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, string> ListVersions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        // IList implementation
+        public T this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EffectsPedalsKeeperTests/Utils/VersionListTests.cs
@@ -0,0 +1,91 @@
+﻿using Xunit;
+
+namespace EffectsPedalsKeeper.Utils.Tests
+{
+    public class VersionListTests
+    {
+        [Fact()]
+        public void CheckOutVersionTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SaveVersionTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SaveAsVersionTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ListVersionsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SaveItemsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ClearTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ContainsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CopyToTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetEnumeratorTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void IndexOfTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InsertTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveAtTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
