20d67997749c971549e57b4751e3ff0e2022e4af
Eric Nerby <ericnerby@msn.com>
2021-02-10 12:31:04 -0500

    add PedalBoard class as subclass of VersionedList

---
+++ EffectsPedalsKeeper/PedalBoard.cs
@@ -0,0 +1,15 @@
+﻿using EffectsPedalsKeeper.Utils;
+using System;
+
+namespace EffectsPedalsKeeper
+{
+    public class PedalBoard : VersionedList<IPedal>
+    {
+        private static IPedal _CopyMethod(IPedal item) => throw new NotImplementedException();
+
+        public PedalBoard(string name) : base(_CopyMethod)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
