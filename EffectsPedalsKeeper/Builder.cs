8d4888f3be28b512551e69f245b07e63a7f8c727
Eric Nerby <ericnerby@msn.com>
2021-02-17 12:04:53 -0500

    allow for basic pedal creation and added all basic menu options

---
+++ EffectsPedalsKeeper/Builder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EffectsPedalsKeeper
+{
+    public class Builder
+    {
+        public static Pedal BuildPedal()
+        {
+            Console.Write("What is the name of the pedal?  ");
+            var name = Console.ReadLine();
+
+            Console.Write("What is the maker of the pedal?  ");
+            var maker = Console.ReadLine();
+
+            EffectType effectType = GetEffectType();
+
+            return new Pedal(maker, name, effectType);
+        }
+
+        private static EffectType GetEffectType()
+        {
+
+            Console.WriteLine("What type of Effect is it?\nChoose a number:");
+            var index = 0;
+            foreach (EffectType type in Enum.GetValues(typeof(EffectType)))
+            {
+                Console.WriteLine($"{index + 1}. {type}");
+                index++;
+            }
+            var input = Console.ReadLine();
+            int typeIndex;
+            if(int.TryParse(input, out typeIndex))
+            {
+                return (EffectType)(typeIndex - 1);
+            }
+            Console.WriteLine("Please select a number from the list.");
+            return GetEffectType();
+        }
+    }
+}
--- EffectsPedalsKeeper/Program.cs
+++ EffectsPedalsKeeper/Program.cs
@@ -5,11 +5,11 @@ namespace EffectsPedalsKeeper
 {
     class Program
     {
-        public static string WelcomeText =
+        static string WelcomeText =
             "Welcome to the Effects Pedals Wrangler.\n"
             + "At any time, type '-q' to quit or '-h' for help.";
 
-        public static string[] MenuItems = 
+        static string[] MenuItems = 
         {
             "View existing pedals",
             "View existing boards",
@@ -17,16 +17,53 @@ namespace EffectsPedalsKeeper
             "Create new board"
         };
 
+        static Action[] MenuActions =
+        {
+            () => ViewExistingPedals(),
+            () => ViewExistingBoards(),
+            () => AddNewPedals(),
+            () => CreateNewBoard()
+        };
+
+        static void ViewExistingPedals()
+        {
+            Console.WriteLine("Existing Pedals");
+        }
+
+        static void ViewExistingBoards()
+        {
+            Console.WriteLine("Existing Boards");
+        }
+
+        static void AddNewPedals()
+        {
+            while(true)
+            {
+                Pedals.Add(Builder.BuildPedal());
+                Console.Write("Would you like to add another pedal? [N/y]  ");
+                var input = Console.ReadLine();
+                if(input.ToLower() != "y")
+                {
+                    break;
+                }
+            }
+        }
+
+        static void CreateNewBoard()
+        {
+            Console.WriteLine("New Board");
+        }
+
+        public static List<Pedal> Pedals = new List<Pedal>();
+        public static List<PedalBoard> PedalBoards = new List<PedalBoard>();
+
         static void Main(string[] args)
         {
-            var pedals = new List<Pedal>();
-            var pedalBoards = new List<PedalBoard>();
-
             Console.WriteLine(WelcomeText);
             InputLoop();
         }
 
-        public static void InputLoop()
+        static void InputLoop()
         {
             while(true)
             {
@@ -40,10 +77,31 @@ namespace EffectsPedalsKeeper
                 {
                     break;
                 }
+                int option;
+                if(int.TryParse(input, out option))
+                {
+                    option -= 1;
+                    if(option >= 0 && option < MenuActions.Length)
+                    {
+                        MenuActions[option]();
+                    }
+                    else
+                    {
+                        Console.WriteLine(
+                            "Please choose a number from the list\n"
+                            + "Type '-q' to quit or '-h' for help");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(
+                        "Please enter your option as a number\n"
+                        + "Type '-q' to quit or '-h' for help");
+                }
             }
         }
 
-        public static bool CheckForQuitOrHelp(string input)
+        static bool CheckForQuitOrHelp(string input)
         {
             input = input.ToUpper();
 
