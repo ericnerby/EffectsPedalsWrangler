fba13c9104873510b97be675101d1cd260b9f570
Eric Nerby <ericnerby@msn.com>
2021-03-02 17:03:49 -0500

    implemented ToString method and wrote input loop for PedalBoard.InteractiveViewEdit()

--- EffectsPedalsKeeper/PedalBoard.cs
+++ EffectsPedalsKeeper/PedalBoard.cs
@@ -1,5 +1,6 @@
 ﻿using EffectsPedalsKeeper.Utils;
 using System;
+using System.Collections.Generic;
 
 namespace EffectsPedalsKeeper
 {
@@ -10,6 +11,62 @@ namespace EffectsPedalsKeeper
 
         public void InteractiveViewEdit(Action<string> checkQuit)
         {
+            Console.WriteLine(Name);
+            if(Count > 0)
+            {
+                Console.Write("Signal Chain:\nGuitar-> ");
+                foreach(Pedal pedal in this)
+                {
+                    Console.Write($"{pedal} ");
+                }
+                Console.Write("->Amp\n");
+            }
+            else
+            {
+                Console.WriteLine("No pedals assigned currently.");
+            }
+
+            while(true)
+            {
+                if (ListVersions().Count > 0)
+                {
+                    Console.WriteLine("Presets:");
+                    foreach (KeyValuePair<int, string> keyValue in ListVersions())
+                    {
+                        Console.WriteLine($"{keyValue.Key + 1}. {keyValue.Value}");
+                    }
+
+                    Console.WriteLine("To view or edit a preset, select a number from the list.");
+                }
+
+                Console.WriteLine("To add, delete, or reorder pedals, enter '-p'");
+                Console.WriteLine("'-a' to add a preset | '-b' to go back to previous screen: ");
+
+                var input = Console.ReadLine();
+                checkQuit(input);
+
+                if (input.ToLower() == "-b") { return; }
+                if (input.ToLower() == "-a")
+                { 
+                    InteractiveNewPreset();
+                    continue;
+                }
+                if (input.ToLower() == "-p")
+                {
+                    InteractiveEditPedals();
+                    continue;
+                }
+                
+            }
+        }
+
+        private void InteractiveEditPedals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InteractiveNewPreset()
+        {
             throw new NotImplementedException();
         }
 
@@ -17,5 +74,7 @@ namespace EffectsPedalsKeeper
         {
             Name = name;
         }
+
+        public override string ToString() => Name;
     }
 }
