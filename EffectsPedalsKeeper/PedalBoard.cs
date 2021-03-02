d49158cfaf9f9f10c06286c557997d04a422bc20
Eric Nerby <ericnerby@msn.com>
2021-03-02 12:27:09 -0500

    set up unit tests and Interface to prepare PedalBoard for interactive editing

--- EffectsPedalsKeeper/PedalBoard.cs
+++ EffectsPedalsKeeper/PedalBoard.cs
@@ -1,11 +1,17 @@
 ﻿using EffectsPedalsKeeper.Utils;
+using System;
 
 namespace EffectsPedalsKeeper
 {
-    public class PedalBoard : VersionedList<Pedal>
+    public class PedalBoard : VersionedList<Pedal>, IInteractiveEditable
     {
         public string Name { get; set; }
         private static Pedal _CopyMethod(Pedal item) => (Pedal)item.Copy();
+
+        public void InteractiveViewEdit(Action<string> checkQuit)
+        {
+            throw new NotImplementedException();
+        }
 
         public PedalBoard(string name) : base(_CopyMethod)
         {
---
+++ EffectsPedalsKeeperTests/PedalBoardTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace EffectsPedalsKeeper.Tests
+{
+    public class PedalBoardTests
+    {
+        private PedalBoard _pedalBoard;
+        private string _boardName = "Cover Band Board";
+
+        public PedalBoardTests()
+        {
+            _pedalBoard = new PedalBoard(_boardName);
+        }
+
+        [Fact()]
+        public void ToStringTest()
+        {
+            var target = _pedalBoard.ToString();
+            var expected = _boardName;
+
+            Assert.Contains(expected, target);
+        }
+    }
+}
