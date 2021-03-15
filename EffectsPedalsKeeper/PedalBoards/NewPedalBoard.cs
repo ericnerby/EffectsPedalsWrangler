0968aa1a280e8b64c2063830cfadb3604eb532d8
Eric Nerby <ericnerby@msn.com>
2021-03-15 11:45:22 -0400

    stubbed fields and tests for NewPedalBoard

--- EffectsPedalsKeeper/PedalBoards/NewPedalBoard.cs
+++ EffectsPedalsKeeper/PedalBoards/NewPedalBoard.cs
@@ -1,10 +1,77 @@
-﻿using System;
+﻿using EffectsPedalsKeeper.Interfaces;
+using EffectsPedalsKeeper.Pedals;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EffectsPedalsKeeper.PedalBoards
 {
-    class NewPedalBoard
+    public class NewPedalBoard : IList<IPedal>, IInteractiveEditable
     {
+        public List<PedalBoardPreset> Presets;
+
+        //IList Implementation
+        public IPedal this[int index] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(IPedal[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<IPedal> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(IPedal item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Interactive Editing
+        public void InteractiveViewEdit(Action<string> checkQuit, Dictionary<string, object> additionalArgs)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
