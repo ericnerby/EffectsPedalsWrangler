4952954cb0136f7b3827826c97380f3b14f4f69a
Eric Nerby <ericnerby@msn.com>
2021-03-15 19:21:28 -0400

    begin writing tests for NewPedalBoard

--- EffectsPedalsKeeper/PedalBoards/NewPedalBoard.cs
+++ EffectsPedalsKeeper/PedalBoards/NewPedalBoard.cs
@@ -1,5 +1,7 @@
 ﻿using EffectsPedalsKeeper.Interfaces;
 using EffectsPedalsKeeper.Pedals;
+using EffectsPedalsKeeper.Settings;
+using EffectsPedalsKeeper.Utils;
 using Newtonsoft.Json;
 using System;
 using System.Collections;
@@ -13,14 +15,57 @@ namespace EffectsPedalsKeeper.PedalBoards
         public string Name { get; set; }
         public List<PedalBoardPreset> Presets { get; private set; }
 
+        public NewPedalBoard(string name)
+        {
+            Name = name;
+            _pedals = new List<IPedal>();
+            Presets = new List<PedalBoardPreset>();
+        }
+
         public NewPedalBoard(string name, IList<IPedal> pedals)
         {
             Name = name;
-            _pedals = new List<IPedal>(pedals);
+            if(pedals.Count > 0) { _pedals = new List<IPedal>(pedals); }
+            else { _pedals = new List<IPedal>(); }
+            Presets = new List<PedalBoardPreset>();
+        }
+
+        public void PresetAdd(string name)
+        {
+            Presets.Add(new PedalBoardPreset(name, _pedals));
+        }
+
+        public bool PresetRemove(PedalBoardPreset preset) => Presets.Remove(preset);
+
+        public void PresetRemoveAt(int index) => Presets.RemoveAt(index);
+
+        private void ExpandPresetOptions(IPedal pedal)
+        {
+
+            foreach (PedalBoardPreset preset in Presets)
+            {
+                var valueKeepers = new List<ValueKeeper<ISetting>>(pedal.Settings.Count);
+                foreach (ISetting setting in pedal.Settings)
+                {
+                    valueKeepers.Add(new ValueKeeper<ISetting>(setting));
+                }
+                preset.SettingValues.AddRange(valueKeepers);
+                preset.EngagedList.Add(pedal, pedal.Engaged);
+            }
+        }
+
+        private void RemovePresetOptions(IPedal pedal)
+        {
+            foreach (PedalBoardPreset preset in Presets)
+            {
+                foreach (ISetting setting in pedal.Settings)
+                {
+                    preset.SettingValues.RemoveAll(value => value.Item == setting);
+                }
+            }
         }
 
         //IList Implementation
-        [JsonProperty]
         private List<IPedal> _pedals;
 
         public IPedal this[int index] { get => _pedals[index]; set => throw new NotImplementedException(); }
@@ -31,10 +76,15 @@ namespace EffectsPedalsKeeper.PedalBoards
 
         public void Add(IPedal item)
         {
-
+            ExpandPresetOptions(item);
+            _pedals.Add(item);
         }
 
-        public void Clear() => _pedals.Clear();
+        public void Clear()
+        {
+            _pedals.Clear();
+            Presets.Clear();
+        }
 
         public bool Contains(IPedal item) => _pedals.Contains(item);
 
@@ -44,25 +94,37 @@ namespace EffectsPedalsKeeper.PedalBoards
         }
 
         public IEnumerator<IPedal> GetEnumerator() => _pedals.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         public int IndexOf(IPedal item) => _pedals.IndexOf(item);
 
         public void Insert(int index, IPedal item)
         {
+            ExpandPresetOptions(item);
             _pedals.Insert(index, item);
         }
 
         public bool Remove(IPedal item)
         {
-            return _pedals.Remove(item);
+            if (_pedals.Remove(item))
+            {
+                RemovePresetOptions(item);
+                return true;
+            }
+            return false;
         }
 
         public void RemoveAt(int index)
         {
+            if (index < 0 || index >= _pedals.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var pedal = _pedals[index];
+            RemovePresetOptions(pedal);
             _pedals.RemoveAt(index);
         }
-
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         //Interactive Editing
         public void InteractiveViewEdit(Action<string> checkQuit, Dictionary<string, object> additionalArgs)
--- EffectsPedalsKeeperTests/PedalBoards/NewPedalBoardTests.cs
+++ EffectsPedalsKeeperTests/PedalBoards/NewPedalBoardTests.cs
@@ -3,11 +3,74 @@ using EffectsPedalsKeeper.PedalBoards;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using EffectsPedalsKeeper.Tests.Mocks;
+using EffectsPedalsKeeper.Pedals;
 
 namespace EffectsPedalsKeeper.PedalBoards.Tests
 {
     public class NewPedalBoardTests
     {
+        private string[] _settingsOptions = new string[]
+            {"6:30", "8:30", "10:30", "12:30", "2:30", "4:30", "5:30"};
+
+        private SettingMock[] _pedalOneSettings;
+        private string[] _pedalOneSettingsLabels = new string[]
+            {"Gain", "Treble", "Output"};
+
+        private SettingMock[] _pedalTwoSettings;
+        private string[] _pedalTwoSettingsLabels = new string[]
+            {"Level", "Tone", "Gain"};
+
+        private PedalMock _testPedalOne;
+        private string _pedalOneName = "Centaur";
+        private string _pedalOneMaker = "Klon";
+        private EffectType _pedalOneType = EffectType.Drive;
+
+        private PedalMock _testPedalTwo;
+        private string _pedalTwoName = "Blues Driver";
+        private string _pedalTwoMaker = "Boss";
+        private EffectType _pedalTwoType = EffectType.Drive;
+
+        private int _startingValue = 3;
+
+        private List<IPedal> _pedals;
+        private string _boardName = "My Awesome PedalBoard";
+        private NewPedalBoard _pedalBoard;
+
+        public NewPedalBoardTests()
+        {
+            _pedalOneSettings = new SettingMock[]
+            {
+                new SettingMock(_pedalOneSettingsLabels[0], _settingsOptions),
+                new SettingMock(_pedalOneSettingsLabels[1], _settingsOptions),
+                new SettingMock(_pedalOneSettingsLabels[2], _settingsOptions)
+            };
+
+            _pedalTwoSettings = new SettingMock[]
+            {
+                new SettingMock(_pedalTwoSettingsLabels[0], _settingsOptions),
+                new SettingMock(_pedalTwoSettingsLabels[1], _settingsOptions),
+                new SettingMock(_pedalTwoSettingsLabels[2], _settingsOptions)
+            };
+
+            _testPedalOne = new PedalMock(_pedalOneName, _pedalOneMaker, _pedalOneType, _pedalOneSettings);
+            _testPedalTwo = new PedalMock(_pedalTwoName, _pedalTwoMaker, _pedalTwoType, _pedalTwoSettings);
+
+            _testPedalOne.Settings.ForEach(setting => setting.CurrentValue = _startingValue);
+            _testPedalTwo.Settings.ForEach(setting => setting.CurrentValue = _startingValue);
+
+            _pedals = new List<IPedal>(2) { _testPedalOne, _testPedalTwo };
+            _pedalBoard = new NewPedalBoard(_boardName, new IPedal[] { _testPedalOne });
+        }
+
+        [Fact()]
+        public void NoPedalsConstructorTest()
+        {
+            var emptyPedalBoard = new NewPedalBoard("Boring Board");
+
+            Assert.Empty(emptyPedalBoard);
+        }
+
         [Fact()]
         public void AddTest()
         {
@@ -58,6 +121,35 @@ namespace EffectsPedalsKeeper.PedalBoards.Tests
 
         [Fact()]
         public void RemoveAtTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void PresetAddTest()
+        {
+            _pedalBoard.PresetAdd("Awesome Preset");
+
+            var expected = _testPedalOne.Settings.Count;
+            var target = _pedalBoard.Presets[0].SettingValues.Count;
+
+            Assert.Equal(expected, target);
+        }
+
+        [Fact()]
+        public void PresetRemoveTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void PresetRemoveAtTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InteractiveViewEditTest()
         {
             Assert.True(false, "This test needs an implementation");
         }
