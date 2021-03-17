dffb1277cf7033050611655300abcfeece0fd879
Eric Nerby <ericnerby@msn.com>
2021-03-17 09:43:22 -0400

    added more pedals to the DemoBuilder to load default pedals on first run

--- EffectsPedalsKeeper/DemoBuilder.cs
+++ EffectsPedalsKeeper/DemoBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using EffectsPedalsKeeper.PedalBoards;
 using EffectsPedalsKeeper.Pedals;
 using EffectsPedalsKeeper.Settings;
-using System;
 using System.Collections.Generic;
 
 namespace EffectsPedalsKeeper
@@ -11,6 +10,10 @@ namespace EffectsPedalsKeeper
         public List<Pedal> DemoPedals = new List<Pedal>() { 
             new Pedal("Ibanez", "TubeScreamer", EffectType.Drive),
             new Pedal("EHX", "Memory Boy", EffectType.Delay),
+            new Pedal("Boss", "CS-2 Compressor Sustainer", EffectType.Drive),
+            new Pedal("EHX", "Electric Mistress", EffectType.Mod),
+            new Pedal("Boss", "DS-1 Distortion", EffectType.Drive),
+            new Pedal("Line 6", "DL4", EffectType.Delay),
         };
 
         public DemoBuilder()
@@ -50,6 +53,54 @@ namespace EffectsPedalsKeeper
                         "Delay",
                     }
                 )
+            );
+
+            DemoPedals[2].AddSettings(
+                Setting.CreateClockFaceSetting("Level", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Attack", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Sustain", "6:30", "5:30")
+            );
+
+            DemoPedals[3].AddSettings(
+                Setting.CreateClockFaceSetting("Rate", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Range", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Color", "6:30", "5:30")
+            );
+
+            DemoPedals[4].AddSettings(
+                Setting.CreateClockFaceSetting("Tone", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Level", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Dist", "6:30", "5:30")
+            );
+
+            DemoPedals[5].AddSettings(
+                new Setting(
+                    "Model Selector", SettingType.Named,
+                    new string[]
+                    {
+                        "Loop Sampler",
+                        "Tube Echo",
+                        "Tape Echo",
+                        "Multi-head",
+                        "Sweep Echo",
+                        "Analog Echo",
+                        "Analog w/ Mod",
+                        "Lo Res Delay",
+                        "Digital Delay",
+                        "Digital w/ Mod",
+                        "Rhythmic Delay",
+                        "Stereo Delays",
+                        "Ping Pong",
+                        "Reverse",
+                        "Dynamic Delay",
+                        "Auto Volume Echo"
+                    }
+                ),
+                Setting.CreateClockFaceSetting("Delay Time", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Repeats", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Tweak", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Tweez", "6:30", "5:30"),
+                Setting.CreateClockFaceSetting("Mix", "6:30", "5:30")
             );
         }
 
