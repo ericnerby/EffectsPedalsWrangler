9c78d2f2b2fcaf77896b7a3c27ae52a7c6e665e9
Eric Nerby <ericnerby@msn.com>
2021-03-22 18:42:25 -0400

    write and test NewInputValidator

---
+++ EffectsPedalsKeeper/CommandLineUtils/NewInputValidator.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EffectsPedalsKeeper.CommandLineUtils
+{
+    public static class NewInputValidator
+    {
+        private static Regex _dashFormat = new Regex(@"^-\w$");
+        private static Regex _intFormat = new Regex(@"^[0-9]+$");
+        private static Regex _doubleFormat = new Regex(@"^\d+\.\d+$");
+        private static Regex _clockFormat = new Regex(@"(\d+):(\d{2})");
+
+        public static InputResponse ParseInput(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return new InputResponse(ResponseType.Empty, null);
+            }
+            Match match;
+            match = _dashFormat.Match(input);
+            if (match.Success)
+            {
+                return new InputResponse(ResponseType.DashOption, match.Value.ToLower());
+            }
+            match = _intFormat.Match(input);
+            if (match.Success)
+            {
+                int value;
+                if (int.TryParse(match.Value, out value))
+                {
+                    return new InputResponse(ResponseType.Int, match.Value);
+                }
+                else
+                {
+                    return new InputResponse(ResponseType.Misc, input);
+                }
+            }
+            match = _doubleFormat.Match(input);
+            if (match.Success)
+            {
+                double value;
+                if (double.TryParse(match.Value, out value))
+                {
+                    return new InputResponse(ResponseType.Double, match.Value);
+                }
+                else
+                {
+                    return new InputResponse(ResponseType.Misc, input);
+                }
+            }
+            match = _clockFormat.Match(input);
+            if (match.Success)
+            {
+                int hour = int.Parse(match.Groups[1].Value);
+                int minute = int.Parse(match.Groups[2].Value);
+                if (hour > 0 && hour <= 12 && minute >= 0 && minute < 60)
+                {
+                    return new InputResponse(ResponseType.TwelveClock, match.Value);
+                }
+                else
+                {
+                    return new InputResponse(ResponseType.Misc, input);
+                }
+            }
+
+            return new InputResponse(ResponseType.Misc, input);
+        }
+    }
+
+    public enum ResponseType
+    {
+        DashOption,
+        Int,
+        Double,
+        TwelveClock,
+        Misc,
+        Empty
+    }
+
+    public class InputResponse
+    {
+        public ResponseType ResponseType { get; set; }
+        public string Value { get; set; }
+
+        public InputResponse(ResponseType responseType, string value)
+        {
+            ResponseType = responseType;
+            Value = value;
+        }
+    }
+}
---
+++ EffectsPedalsKeeperTests/CommandLineUtils/NewInputValidatorTests.cs
@@ -0,0 +1,124 @@
+﻿using Xunit;
+using EffectsPedalsKeeper.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectsPedalsKeeper.CommandLineUtils.Tests
+{
+    public class NewInputValidatorTests
+    {
+        [Fact()]
+        public void ParseInputEmptyTest()
+        {
+            var testString = "";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Empty, response.ResponseType);
+            Assert.Null(response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputDashTest()
+        {
+            var testString = "-B";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.DashOption, response.ResponseType);
+            Assert.Equal(testString.ToLower(), response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputIntTest()
+        {
+            var testString = "5";
+            var target = "5";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Int, response.ResponseType);
+            Assert.Equal(target, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputDoubleTest()
+        {
+            var testString = "5.4";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Double, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputClockTest()
+        {
+            var testString = "12:35";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.TwelveClock, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputMinutesAboveSixtyTest()
+        {
+            var testString = "12:75";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Misc, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputZeroHourTest()
+        {
+            var testString = "0:35";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Misc, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputMilitaryTimeTest()
+        {
+            var testString = "14:05";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Misc, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputClockMinTest()
+        {
+
+            var testString = "1:00";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.TwelveClock, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputClockMaxTest()
+        {
+
+            var testString = "12:59";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.TwelveClock, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+
+        [Fact()]
+        public void ParseInputMiscTest()
+        {
+            var testString = "Something-Different 15.3";
+            var response = NewInputValidator.ParseInput(testString);
+
+            Assert.Equal(ResponseType.Misc, response.ResponseType);
+            Assert.Equal(testString, response.Value);
+        }
+    }
+}
