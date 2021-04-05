c824d973463a36dc1b2d0bf73abccef22ab6fe7d
Eric Nerby <ericnerby@msn.com>
2021-04-05 16:28:08 -0400

    added Context class

---
+++ EffectsPedalsKeeperShared/Data/Context.cs
@@ -0,0 +1,25 @@
+﻿using EffectsPedalsKeeperShared.Models;
+using EffectsPedalsKeeperShared.Models.Settings;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectsPedalsKeeperShared.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Pedal> Pedals { get; set; }
+        public DbSet<PedalBoard> PedalBoards { get; set; }
+        public DbSet<PedalBoardPedal> PedalBoardPedals { get; set; }
+        public DbSet<PedalPreset> PedalPresets { get; set; }
+        public DbSet<Preset> Presets { get; set; }
+        public DbSet<SettingPreset> SettingPresets { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<OptionSetting> OptionSettings { get; set; }
+    }
+}
+
