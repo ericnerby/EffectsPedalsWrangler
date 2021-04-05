05cf636ebaeb5fa5b360d73af928f77f161f407f
Eric Nerby <ericnerby@msn.com>
2021-04-05 12:11:20 -0400

    add PedalBoardPedal to models

---
+++ EffectsPedalsKeeperShared/Models/PedalBoardPedal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EffectsPedalsKeeperShared.Models
+{
+    public class PedalBoardPedal
+    {
+        public int Id { get; set; }
+        public int PedalBoardId { get; set; }
+        public int PedalId { get; set; }
+        public int Order { get; set; }
+
+        public PedalBoard PedalBoard { get; set; }
+        public Pedal Pedal { get; set; }
+    }
+}
